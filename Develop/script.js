1ea86fb550f5f3ae1b1bbb19fd861cccd0783b8a
mvfranzke <michellevonfranzke@gmail.com>
2023-05-25 06:30:14 -0700

    added array of special characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,44 @@
-// Assignment Code
+/* List of special charaters to be included in password when user answers Ok to include special character on the prompt */
+var specialCharacters = [
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
+  "'",
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
+];
+
+
+
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);
+
+
